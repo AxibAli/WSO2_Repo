755be12fd4fe6b21532b5a415bae07b623670819
Faisal-Ilyas <faisal.ilyas@evantagesoft.com>
2023-08-01 16:56:21 +0500

    wso2 api  manager init++

---
+++ wso2am-4.1.0/wso2am-4.1.0/repository/deployment/server/jaggeryapps/devportal/site/public/theme/userTheme.js
@@ -0,0 +1,37 @@
+// const Configurations = {
+//     /*
+//      This file can be used to override the configurations in devportal/source/src/defaultTheme.js
+//      ex. Uncomment the below section to enable the landingPage
+//      */
+//      /*
+//      custom: {
+//          landingPage: {
+//              active: true,
+//          },
+//      },
+//      */
+// };
+
+const Configurations=
+    {
+        "custom": {
+            "apiDetailPages": {
+                "showCredentials": true,
+                "showComments": true,
+                "showTryout":false,
+                "showDocuments": true,
+                "showSdks": true, 
+                "onlyShowSdks": []
+            },
+            "appBar": {
+                "logo": "/site/public/images/zong_logo.svg",
+                "logoHeight": 40,
+                "logoWidth": 150,
+                "background": "#3ba1c5",
+                "activeBackground": "#ffd500"
+            }
+            
+        }
+        
+        
+    };
